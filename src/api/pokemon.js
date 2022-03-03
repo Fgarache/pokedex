c00a49c3deed03759348f7ed4939b477266db057
Francisco Garache <franciscogarach3@gmail.com>
2022-03-03 17:51:31 -0600

    video numero 6 (master)

---
+++ src/api/pokemon.js
@@ -0,0 +1,13 @@
+import { API_HOST } from "../utils/constants";
+
+export async function getPokemonsApi() {
+  try {
+    const url = `${API_HOST}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
